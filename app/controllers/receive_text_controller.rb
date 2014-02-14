f24cb4471927e777ca6fa973f0c929c5f8b2e4a2
mkvisakan <mails4visak@gmail.com>
2014-02-14 15:19:16 -0800

    send and receive messages and API integration

--- app/controllers/receive_text_controller.rb
+++ app/controllers/receive_text_controller.rb
@@ -1,3 +1,6 @@
+require 'open-uri'
+require 'json'
+
 class ReceiveTextController < ApplicationController
   def index 
     # let's pretend that we've mapped this action to 
@@ -5,6 +8,29 @@ class ReceiveTextController < ApplicationController
     
     message_body = params["Body"]
     from_number = params["From"]
+
+    puts from_number
+    puts message_body
+
+    render text: "Thank you! You will receive an SMS shortly with bus timings."
+  
+    msg_parts = message_body.split().map {|s| s.to_i}
+    stop_no = msg_parts[0]
+    bus_no = msg_parts[1]
+
+    sms_api_url = "http://api.smsmybus.com/v1/getarrivals?key=bontrager&stopID=#{stop_no}"
+    puts sms_api_url
+
+    url_open = open(sms_api_url)
+    json_obj = JSON.load(url_open)
+
+    to_msg = ""
+
+    for elt in json_obj["stop"]["route"]
+        if elt['routeID'].to_i == bus_no
+           to_msg += "#{elt['routeID']}  #{elt['arrivalTime']}\n"
+        end
+    end
  
     twilio_sid = 'AC15a225ec77a2891ead8403d67723d2d0'
     twilio_token = "f1bffe6a8d0a28e9b6068a983cb3a99b"
@@ -15,7 +41,7 @@ class ReceiveTextController < ApplicationController
     @twilio_client.account.sms.messages.create(
       :from => "+1#{twilio_phone_number}",
       :to => from_number ,
-      :body => "This is an message. It gets sent to #{from_number}"
+      :body => to_msg
     )
  
   end
